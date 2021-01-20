9d1bb183e0ae3e88ea538e364e53ecff805b5d1d
Casper <casperdeekie@hotmail.com>
2021-01-20 16:05:32 +0100

    Can now submit guess again, can submit rating, the game is now completely playable.

--- myapp/public/javascripts/gamePage.js
+++ myapp/public/javascripts/gamePage.js
@@ -4,14 +4,14 @@ let gameID = null;
 
 //Hides the board until the game starts.
 const board = document.querySelector(".board");
-board.hidden = true;
+board.style.visibility = "hidden";
 let guessesLeft = 10;
 let player = null;
 
-let currentColour;
+const holesCG = document.querySelectorAll(".currentGuess > .hole");
 
-// const red = document.querySelector(".red");
-// const hole = document.getElementById("aa");
+
+let currentColour;
 
 const guessHoles = document.querySelectorAll(".currentGuess > div");
 guessHoles.forEach(addEventListenersHoles);
@@ -26,7 +26,21 @@ function addEventListenersHoles(item, index) {
 
     item.addEventListener("drop", function(event){
         event.preventDefault();
-        event.target.style.backgroundColor = currentColour;
+        if(document.querySelector(".guessesLeft").textContent != "N/A" && player == 1){
+            if(event.target.style.backgroundColor == "red" && currentColour == "red"){
+                currentColour = "maroon";
+            }
+            if(event.target.style.backgroundColor == "yellow" && currentColour == "yellow"){
+                currentColour = "orange";
+            }
+            if(event.target.style.backgroundColor == "green" && currentColour == "green"){
+                currentColour = "darkgreen";
+            }
+            event.target.style.border = "10px solid " + currentColour; 
+        }
+        else{
+            event.target.style.backgroundColor = currentColour;
+        }
     })
 }
 
@@ -36,32 +50,20 @@ function addEventListenersPins(item, index) {
     })
 }
 
-// red.addEventListener("dragstart", function(event){
-//     currentColour = event.target.className;
-// })
-
-// hole.addEventListener("dragover", function(event){
-//     event.preventDefault();
-// })
-
-// hole.addEventListener("drop", function(event){
-//     event.preventDefault();
-//     event.target.style.backgroundColor = currentColour;
-// })
 
 window.onbeforeunload = function(){
     socket.send("GameID = " + gameID + ", Aborting game.");
  }
 
-/*
-//document.querySelector(".guessSubmit").onclick = function() {
+
+document.querySelector(".guessSubmit").onclick = function() {
     //If player 2 submits their guess, puts all 4 pins into an array and sends it to the server (which sends it to player 1)
     //We are also using the currentGuess holes for player 1 to make a combination.
-    let hole1 = document.getElementById("hole1");
-    let hole2 = document.getElementById("hole2");
-    let hole3 = document.getElementById("hole3");
-    let hole4 = document.getElementById("hole4");
-    let guess = [hole1.value, hole2.value, hole3.value, hole4.value];
+    let hole1 = holesCG[0].style.backgroundColor;
+    let hole2 = holesCG[1].style.backgroundColor;
+    let hole3 = holesCG[2].style.backgroundColor;
+    let hole4 = holesCG[3].style.backgroundColor;
+    let guess = [hole1, hole2, hole3, hole4];
 
     //If the player is player1, then instead of sending these pins as a guess, send it as a combination.
     //The only difference in code between submitting as p1 and p2 is that p1 sends a slightly different message to the server, and nothing is added to the prevGuesses.
@@ -72,6 +74,13 @@ window.onbeforeunload = function(){
          //Hides the guessSubmit button from player 1 and disables the guess form.
          disableEveryThing();
 
+         pins[1].className = "yellow";
+         pins[2].className = "green";
+
+         for(let i = 0; i < 5; i++){
+             pins[0].parentElement.removeChild(pins[0].parentElement.lastElementChild);
+         }
+
         //Update status
         target.innerHTML = "Waiting for opponent to make a guess...";
         document.querySelector(".guessesLeft").textContent = guessesLeft;
@@ -80,12 +89,10 @@ window.onbeforeunload = function(){
         let whichHoles = 10 - guessesLeft;
         let selectWhichHoles = ".prevGuess" + whichHoles + " .hole";
         let holes = document.querySelectorAll(selectWhichHoles);
-        holes[0].innerHTML = guess[0];
-        holes[1].innerHTML = guess[1];
-        holes[2].innerHTML = guess[2];
-        holes[3].innerHTML = guess[3];    
-
-        disableEveryThing();
+        holes[0].style.backgroundColor = guess[0];
+        holes[1].style.backgroundColor = guess[1];
+        holes[2].style.backgroundColor = guess[2];
+        holes[3].style.backgroundColor = guess[3];    
 
         guessesLeft--;
 
@@ -96,17 +103,41 @@ window.onbeforeunload = function(){
 
         socket.send("GameID = " + gameID + ", Guess = " + guess.toString());
     }
+    holesCG[0].style.backgroundColor = "black";
+    holesCG[1].style.backgroundColor = "black";
+    holesCG[2].style.backgroundColor = "black";
+    holesCG[3].style.backgroundColor = "black";
+    
+    disableEveryThing();
 }
-*/
 
-document.querySelector(".rateGuessSubmit").onclick = function() {
+
+document.querySelector(".ratingSubmit").onclick = function() {
     //If player 1 submits their rating of the guess, all 4 values are added to a total value (where green = 5, yellow = 1, red = 0)
     //This is sent to the server, which then forwards it to the client. Then the total is disected again into the original values.
-    let ratings = document.querySelectorAll("[id = rating]");
     let totalRating = 0;
-    totalRating += parseInt(ratings[0].value) + parseInt(ratings[1].value) + parseInt(ratings[2].value) + parseInt(ratings[3].value);
+    for(let i = 0; i < 4; i++){
+        if(holesCG[i].style.borderColor == "green" || holesCG[i].style.borderColor == "darkgreen"){
+            totalRating += 5;
+            continue;
+        }
+        else if(holesCG[i].style.borderColor == "yellow" || holesCG[i].style.borderColor == "orange" ){
+            totalRating++;
+            continue;
+        }
+    }
     socket.send("GameID = " + gameID + ", Rating = " + totalRating);
-    document.querySelector(".rateGuess").hidden = true;
+    disableEveryThing();
+
+    holesCG[0].style.backgroundColor = "black";
+    holesCG[1].style.backgroundColor = "black";
+    holesCG[2].style.backgroundColor = "black";
+    holesCG[3].style.backgroundColor = "black";
+
+    holesCG[0].style.borderStyle = "none";
+    holesCG[1].style.borderStyle = "none";
+    holesCG[2].style.borderStyle = "none";
+    holesCG[3].style.borderStyle = "none";
 
     //Update status
     target.innerHTML = "Waiting for opponent to make a guess...";
@@ -137,13 +168,13 @@ socket.onmessage = function(event) {
     if(event.data.includes("game is starting...")){
         //When the game starts, un-hides the board, for player 1, so he can create the combination.
         if(player == 1){
-            board.hidden = false;
+            board.style.visibility = "visible";
         }
     }
 
     if(event.data.includes("Start guessing.")){
         //Unhides the board for player 2 when player 1 has made a combination and the game can start.
-        board.hidden = false;
+        board.style.visibility = "visible";
 
         //Update status
         target.innerHTML = "Make a guess.";
@@ -151,7 +182,6 @@ socket.onmessage = function(event) {
     }
 
     if(event.data.includes("Create a combination.")){
-        document.querySelector(".currentGuessText").hidden = false;
         //document.querySelector(".guessSubmit").hidden = false;
 
         //Update status
@@ -183,19 +213,14 @@ socket.onmessage = function(event) {
         let guess = event.data.substring(8); 
         let guessArray = guess.split(",");
 
-        const rateGuess = document.querySelector(".rateGuess");
         const rateGuessSpan = document.querySelectorAll(".rateGuessShow");
 
-        const defaultRatings = document.querySelectorAll(".redRating");
-        for(let i = 0; i < 4; i++){
-            defaultRatings[i].selected = "selected";
-        }
+        holesCG[0].style.backgroundColor = guessArray[0];
+        holesCG[1].style.backgroundColor = guessArray[1];
+        holesCG[2].style.backgroundColor = guessArray[2];
+        holesCG[3].style.backgroundColor = guessArray[3];
 
-        rateGuess.hidden = false;
-        rateGuessSpan[0].textContent = guessArray[0];
-        rateGuessSpan[1].textContent = guessArray[1];
-        rateGuessSpan[2].textContent = guessArray[2];
-        rateGuessSpan[3].textContent = guessArray[3];
+        enableEveryThing();
 
         //Update status
         target.innerHTML = "Rate the guess.";
@@ -206,10 +231,10 @@ socket.onmessage = function(event) {
         let whichHoles = 9 - guessesLeft;
         let selectWhichHoles = ".prevGuess" + whichHoles + " .hole";
         let holes = document.querySelectorAll(selectWhichHoles);
-        holes[0].innerHTML = guessArray[0];
-        holes[1].innerHTML = guessArray[1];
-        holes[2].innerHTML = guessArray[2];
-        holes[3].innerHTML = guessArray[3];    
+        holes[0].style.backgroundColor = guessArray[0];
+        holes[1].style.backgroundColor = guessArray[1];
+        holes[2].style.backgroundColor = guessArray[2];
+        holes[3].style.backgroundColor = guessArray[3];    
     }
 
     if(event.data.includes("GuessesLeft = ")){
@@ -223,20 +248,19 @@ socket.onmessage = function(event) {
         //The same thing goes for if total rating >= 1, then there has to be at leat 1 yellow.
         //This is so that the player knows how many greens/yellows/reds he got, but not which rating belonged to which pin.
         let ratingTotal = parseInt(event.data.substring(9));
-        let ratingSplit = "";
+        let ratingSplit = ["undefined", "undefined", "undefined", "undefined"];
         for(var i = 0; i < 4; i++){
             if(ratingTotal >= 5){
-                ratingSplit += "Green";
+                ratingSplit[i] = "green";
                 ratingTotal -= 5;
             }
             else if(ratingTotal >= 1){
-                ratingSplit += "Yellow";
+                ratingSplit[i] = "yellow";
                 ratingTotal -= 1;
             }
             else{
-                ratingSplit += "Red";
+                ratingSplit[i] = "red";
             }
-            if(i < 3) ratingSplit += ", "
         }
 
         if(player == 2){
@@ -248,7 +272,9 @@ socket.onmessage = function(event) {
         //Select the right rating out of the 10 previous guess slots that are on the board.
         let whichPrevGuess = 9 - guessesLeft;
         let selectRightPrevGuess = ".prevGuess" + whichPrevGuess + " .rating";
-        document.querySelector(selectRightPrevGuess).innerHTML = "Rating: " + ratingSplit;
+        for(let i = 0; i < 4; i++){
+            document.querySelector(selectRightPrevGuess + (i+1)).style.backgroundColor = ratingSplit[i];
+        }
     }
 
     if(event.data.includes("Player 1 won")){
@@ -258,8 +284,8 @@ socket.onmessage = function(event) {
         else{
             //When the game is finished, disable the guess form for player 2. 
             target.innerHTML = "You lose!\nThe combination was: " + event.data.substring(28);
-            disableEveryThing();
         }
+        disableEveryThing();
     }
 
     if(event.data.includes("Player 2 won")){
@@ -269,8 +295,8 @@ socket.onmessage = function(event) {
         else{
             //When the game is finished, disable the guess form for player 2.
             target.innerHTML = "You win!";
-            disableEveryThing();
         }
+        disableEveryThing();
     }
 
     if(event.data.includes("Game was aborted")){
@@ -286,19 +312,60 @@ socket.onopen = function() {
 
 function disableEveryThing(){
     //Stops player from guessing by disabling the whole form.
-    //document.querySelector(".guessSubmit").hidden = true;
-    document.querySelector(".currentGuessText").hidden = true;
-    document.getElementById("hole1").disabled = true;
-    document.getElementById("hole2").disabled = true;
-    document.getElementById("hole3").disabled = true;
-    document.getElementById("hole4").disabled = true;
+    document.querySelector(".guessSubmit").hidden = true;
+    document.querySelector(".ratingSubmit").hidden = true;
+    pins[0].draggable = false;
+    pins[1].draggable = false;
+    pins[2].draggable = false;
+    if(player == 2){
+        pins[3].draggable = false;
+        pins[4].draggable = false;
+        pins[5].draggable = false;
+        pins[6].draggable = false;
+        pins[7].draggable = false;
+    }
 }
 
 function enableEveryThing(){
     //Re-enables form so player can guess again.
-    //document.querySelector(".guessSubmit").hidden = false;
-    document.getElementById("hole1").disabled = false;
-    document.getElementById("hole2").disabled = false;
-    document.getElementById("hole3").disabled = false;
-    document.getElementById("hole4").disabled = false;
+    let whichButtonEnable = ".guessSubmit";
+    if(player == 1){
+        whichButtonEnable = ".ratingSubmit";
+    }
+    document.querySelector(whichButtonEnable).hidden = false;
+    pins[0].draggable = true;
+    pins[1].draggable = true;
+    pins[2].draggable = true;
+    if(player == 2){
+        pins[3].draggable = true;
+        pins[4].draggable = true;
+        pins[5].draggable = true;
+        pins[6].draggable = true;
+        pins[7].draggable = true;
+    }
 }
+
+
+//Set button position.
+const currentGuess = document.querySelector(".currentGuess");
+let currentGuessX = currentGuess.getBoundingClientRect().left;
+let currentGuessY = currentGuess.getBoundingClientRect().top;
+let buttonX = (currentGuessX + currentGuess.offsetWidth) * 1.05;
+let buttonY = currentGuessY + currentGuess.offsetHeight/2 - document.querySelector(".guessSubmit").offsetHeight/2;
+document.querySelector(".guessSubmit").style.margin = buttonY + "px auto auto " + buttonX + "px";
+document.querySelector(".ratingSubmit").style.margin = buttonY + "px auto auto " + buttonX + "px";
+document.querySelector(".ratingSubmit").hidden = true
+document.querySelector(".board").hidden = true;
+
+
+window.onresize = function() {
+    const currentGuess = document.querySelector(".currentGuess");
+    let currentGuessX = currentGuess.getBoundingClientRect().left;
+    let currentGuessY = currentGuess.getBoundingClientRect().top;
+
+    let buttonX = (currentGuessX + currentGuess.offsetWidth) * 1.05;
+    let buttonY = currentGuessY + currentGuess.offsetHeight/2 - document.querySelector(".guessSubmit").offsetHeight/2;
+    document.querySelector(".ratingSubmit").style.margin = buttonY + "px auto auto " + buttonX + "px";
+    document.querySelector(".guessSubmit").style.margin = buttonY + "px auto auto " + buttonX + "px";
+}
+
